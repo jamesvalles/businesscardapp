a8cff58f68e3638f4f2287869053ca7c0b2ced3f
James Valles <jamesvallesdotcom@gmail.com>
2019-11-12 20:49:53 -0600

    Added authentication to login page

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import * as firebase from 'firebase/app'
+import { AngularFireModule } from '@angular/fire';
+import {environment} from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private angularFireAuth : AngularFireModule) {
+    firebase.initializeApp(environment.firebase);
+   }
+
+   signIn(email: string, password: string) {
+     console.log("Authenticated has started.")
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('Successfully signed in!');
+      })
+      .catch(err => {
+        console.log('Something is wrong:',err.message);
+      });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authservice : AuthenticationService) {
+
+   }
 
   ngOnInit() {
   }
 
+  login(f : NgForm){
+    console.log("Sign-in button pressed.");
+    console.log("Credentials: " + f.controls['email'].value +", " + f.controls['password'].value);
+    this._authservice.signIn(f.controls['email'].value, f.controls['password'].value);
+    f.reset();
+  }
+
+  signup(){
+    console.log("Sign-up button pressed.")
+  }
 }
+
